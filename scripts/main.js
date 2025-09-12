143b2b6b00fd6c1ab2bb9bb7b7d2e1265a5718ef
andre <11andrecarvalho@gmail.com>
2025-09-12 20:29:25 -0300

    minor fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
-
-
-
-
-// <-------- working on sidebar open and close functions ------------>
+// <------ Global variables -------->
 
 let header_sidebar_open_icon = document.getElementById("open-sidebar-icon");
 let sidebar_close_icon = document.getElementById("close-sidebar-icon");
 let sidebar = document.getElementById("sidebar-narrow");
+let togglebtn = document.getElementById("page-mode");
+let prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+let under_construction_img = document.getElementById("under-construction-img");
 
-// --> Show sidebar functionality
+
+// <----------- Events ------------->
+
+// Show sidebar functionality
 header_sidebar_open_icon.addEventListener('click', () => {
     
     sidebar.style.right = "0";
@@ -16,45 +18,29 @@ header_sidebar_open_icon.addEventListener('click', () => {
 
 });
 
-// --> Close sidebar functionality
+// Close sidebar functionality
 sidebar_close_icon.addEventListener('click', () => {
     
     sidebar.style.right = "-15rem";
     sidebar.style.opacity = "0";
 });
 
-
-// <------------------------------------------------------------------->
-
-
-
-
-// <---------------- Light/Dark mode toggle [START] --------------------------->
-
-
-let togglebtn = document.getElementById("page-mode");
-let mode = (window.matchMedia("(prefers-color-scheme: dark)")['matches']) ? "dark" : "light";
-
-document.addEventListener("load", () => {
-});
-
-
-
+// Toggle Light/Dark mode
 togglebtn.addEventListener('click', () => {
     // TODO: implement logic for toggle mode
-    if (mode == "dark"){
-        mode = "light";
+    if (prefersDark['matches'] == true){
         localStorage.setItem("setmode", "light");
     } else{
-        mode = "dark";
         localStorage.setItem("setmode", "dark");
     }
 
 });
 
+// Changes content when user pref for light/dark mode (browser/system-wide) changes
+prefersDark.addEventListener("change", () => {
+    under_construction_img.setAttribute('src', (prefersDark['matches'] == true) ? "../assets/img/under-construction-dark.png" : "../assets/img/under-construction-light.png");
+});
 
-// <---------------- Light/Dark mode toggle [END] --------------------------->
-
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    under_construction_img.setAttribute('src', (prefersDark['matches'] == true) ? "../assets/img/under-construction-dark.png" : "../assets/img/under-construction-light.png");
+})
