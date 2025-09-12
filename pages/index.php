0fb7b872c22f01f94c377d2f5e42482428e92786
andre <11andrecarvalho@gmail.com>
2025-09-12 07:42:22 -0300

    Removed html, body, head tags from header.php include file. Added favicon -> Fixes #4

--- pages/index.php
+++ pages/index.php
@@ -7,10 +7,13 @@
 		<meta name="keywords" content="portfolio, developer, hire, freelance, showcase">
 		<meta name="author" content="Andre Carvalho Filho">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 		<title>Initial Page</title>
+		<link rel="stylesheet" href="../styles/main.css">
 	</head>
 	<body>
 		<?php include '../includes/header.php';?>
+
+
+		<script src="../scripts/main.js"></script>
 	</body>
 </html>
