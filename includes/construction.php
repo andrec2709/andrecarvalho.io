e1bf74a0a1238008e5e9e684e2e055b4084b0ba1
andre <11andrecarvalho@gmail.com>
2025-09-12 21:06:18 -0300

    Implemented dark/light mode toggle

--- includes/construction.php
+++ includes/construction.php
@@ -6,7 +6,7 @@ include "../lang/$language.php";
 ?>
 
 <div id="under-construction-container">
-<h1 class="h-under-construction under-construction-txt"><?=$lang['thank you']?></h1>
+<h1 class="h-under-construction under-construction-txt theme-dependant"><?=$lang['thank you']?></h1>
 <img id="under-construction-img" src="../assets/img/under-construction-dark.png" alt="Image showing that this page is not yet ready">
-<p class="p-under-construction under-construction-txt"><?=$lang['construction']?></p>
+<p class="p-under-construction under-construction-txt theme-dependant"><?=$lang['construction']?></p>
 </div>
--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,10 @@ let sidebar_close_icon = document.getElementById("close-sidebar-icon");
 let sidebar = document.getElementById("sidebar-narrow");
 let togglebtn = document.getElementById("page-mode");
 let prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+let current_mode = prefersDark.matches ? "dark" : "light";
+
 let under_construction_img = document.getElementById("under-construction-img");
+
 
 
 // <----------- Events ------------->
@@ -28,17 +31,39 @@ sidebar_close_icon.addEventListener('click', () => {
 // Toggle Light/Dark mode
 togglebtn.addEventListener('click', () => {
     // TODO: implement logic for toggle mode
-    if (prefersDark['matches'] == true){
-        localStorage.setItem("setmode", "light");
-    } else{
-        localStorage.setItem("setmode", "dark");
-    }
 
+    let elements = document.getElementsByClassName("theme-dependant");
+    let changeEvent = new Event("change")
+    
+    if (current_mode == "light"){
+        current_mode = "dark";
+        document.body.style.backgroundColor = "var(--main-bg-color-dark)";
+        prefersDark.dispatchEvent(changeEvent);
+
+        localStorage.setItem("setmode", "light");
+        for (index = 0; index < elements.length; index++){
+            elements.item(index).classList.remove("light");
+            elements.item(index).classList.add("dark");
+            console.log(elements.item(index).classList);
+    };
+    } else{
+        current_mode = "light";
+        document.body.style.backgroundColor = "var(--main-bg-color-light)";
+        prefersDark.dispatchEvent(changeEvent);
+
+        localStorage.setItem("setmode", "dark");
+        for (index = 0; index < elements.length; index++){
+            elements.item(index).classList.remove("dark");
+            elements.item(index).classList.add("light");
+            console.log(elements.item(index).classList);
+        }
+    }
 });
 
 // Changes content when user pref for light/dark mode (browser/system-wide) changes
 prefersDark.addEventListener("change", () => {
-    under_construction_img.setAttribute('src', (prefersDark['matches'] == true) ? "../assets/img/under-construction-dark.png" : "../assets/img/under-construction-light.png");
+    under_construction_img.setAttribute('src', (current_mode == "dark") ? "../assets/img/under-construction-dark.png" : "../assets/img/under-construction-light.png");
+    console.log("changed");
 });
 
 document.addEventListener("DOMContentLoaded", () => {
