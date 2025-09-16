890f2e64193e4c121fa497bcefe5f46dabda8643
andre <11andrecarvalho@gmail.com>
2025-09-16 11:02:05 -0300

    testing stuff on live server

--- scripts/main.js
+++ scripts/main.js
@@ -99,7 +99,7 @@ async function onSubmit(token){
 
 if (isPage("contact.php")){
 
-document.getElementById("contact-me-form").addEventListener("submit", e => {
+document.getElementById("contact-me-form").addEventListener("submit", function(e) {
 
     e.preventDefault();
     
